07457e070cde5ada477cda8f0ac1c3157a924758
BiYing <bp483@drexel.edu>
2023-11-03 14:35:01 -0400

    added Subsequence

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test {
+	public static void main(String[] args){
+		// case 1
+		Integer i1 = 130;
+		Integer i2 = 130;
+		System.out.println(i1 == i2);
+
+		// case 2
+		int i3 = 1000;
+		int i4 = 1000;
+		System.out.println(i3 == i4);
+
+		//case 3
+		Integer i5 = 127;
+		Integer i6 = 127;
+		System.out.println(i5 == i6);
+	}
+}
