a86c1ea2d548dcc5642d6288cf1a4ef1dfbfa1d8
BiYing <bp483@drexel.edu>
2024-06-04 10:22:19 -0400

    add binary

---
+++ CloseStrings.java
@@ -0,0 +1,29 @@
+// Determine if Two Strings are Close
+
+// Two string are considered close if you can attain one from the other using the following operations:
+
+// Operation 1: Swap any two existing characters.
+// 	For Example, abcde -> aecdb
+
+// Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character. 
+// 	For Example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+
+// You can use the operations on either string as many times as necessary.
+
+// Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+// Example 1:
+// Input: word1 = "abc", word2 = "bca"
+// Output: true
+// Explanation: You can attain word2 from word1 in 2 operations
+// Operation 1: "abc" -> "acb"
+// Operation 2: "acb" -> "bca"
+
+public CloseStrings{
+	public static boolean closeStrings(String word1, String word2){
+		
+	}
+	public static void main (String[] args){
+
+	}
+}
